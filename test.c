3f0025b616091bdeae3b72cf25ddec2c5a0b9067
eggman <skouhei@gmail.com>
2017-06-17 16:13:24 +0900

    check size at zm_malloc().

--- test.c
+++ test.c
@@ -3,31 +3,45 @@
 #include "zmpool.h"
 #include "zmpool_i.h"
 
+void is_null(void *p)
+{
+    if(p == NULL)
+        printf("pass\n");
+    else
+        printf("fail\n");
+}
+
+void is_notnull(void *p)
+{
+    if(p != NULL)
+        printf("pass\n");
+    else
+        printf("fail\n");
+}
+
 int main(void)
 {
    int *p = NULL;
 
    p = zm_malloc(sizeof(int));
-
-   if(p != NULL)
-     printf("pass\n");
-   else
-     printf("fail\n");
+   is_notnull(p);
 
    *p = 1;
-
    if(*p == 1)
-     printf("pass\n");
+        printf("pass\n");
    else
-     printf("fail\n");
+        printf("fail\n");
+
+   p = zm_malloc(0);
+   is_null(p);
+
+   p = zm_malloc(POOL_BLOCK_SIZE + 1);
+   is_null(p);
 
    for(uint32_t i=0; i<POOL_SIZE; i++)
       p = zm_malloc(sizeof(int));
 
-   if(p == NULL)
-     printf("pass\n");
-   else
-     printf("fail\n");
+   is_null(p);
 
    zm_free(p);
    p = NULL;
--- zmpool.c
+++ zmpool.c
@@ -11,6 +11,9 @@ void *zm_malloc(size_t size)
     static uint32_t used = 0;
     void *p = NULL;
 
+    if(size == 0 || size > POOL_BLOCK_SIZE)
+        return (void *)NULL;
+
     if(used < POOL_SIZE)
         p = (void *) &zm_pool[used++ * POOL_BLOCK_SIZE];
 
